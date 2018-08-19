276d6de955b83e2609c737401071f06e1379df3f
Dogy06 <weichen.huang.06@gmail.com>
2018-08-19 13:25:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,12 +32,5 @@ training_data = array([[0, 1, 1, 0]]).T
 
 mlp.train_weights(training_data, inputs)
 
-new_input = array((input('X:'), input('Y:'), input('Z:')), dtype=int)
-new_input_result = mlp.feedforward(new_input)
 
-print()
-print("New input values: ")
-print(new_input)
-print()
-print("New input result: ")
-print(new_input_result)
+
